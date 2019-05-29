b0323cf59546d7810f72d2bac9437f962d7fa3e2
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-05-29 16:35:11 +0200

    Implement version string utility.

---
+++ tests/unit/utils/test_version.py
@@ -0,0 +1,98 @@
+"""
+Tests for `vinegar.utils.version`.
+"""
+
+import os.path
+import pathlib
+import time
+import unittest
+
+from tempfile import TemporaryDirectory
+
+from vinegar.utils.version import (
+    aggregate_version, version_for_file_path, version_for_str)
+
+class TestVersionModule(unittest.TestCase):
+    """
+    Tests for the `vinegar.utils.version`.
+    """
+
+    def test_aggregate_version(self):
+        """
+        Test that an aggregate version can be calculated using
+        `aggregate_version`.
+        """
+        # Generated version strings should not be empty and they should not
+        # match for different input, even though the latter one is not a strict
+        # requirement because due to the use of hashes, there could be
+        # accidental collisions.
+        version1 = aggregate_version(['1', '2', '3'])
+        self.assertTrue(len(version1) > 0)
+        version2 = aggregate_version(['1', '2'])
+        self.assertTrue(len(version2) > 0)
+        self.assertNotEqual(version1, version2)
+        version3 = aggregate_version(['4', '2', '3'])
+        self.assertTrue(len(version3) > 0)
+        self.assertNotEqual(version1, version3)
+        self.assertNotEqual(version2, version3)
+
+    def test_version_for_file_path(self):
+        """
+        Test the `version_for_file_path` function.
+        """
+        with TemporaryDirectory() as tmpdir:
+            test_file_path = os.path.join(tmpdir, 'test')
+            # First, we test that generating a version number for a file, that
+            # does not exist, does not result in an exception.
+            version1 = version_for_file_path(test_file_path)
+            self.assertTrue(len(version1) > 0)
+            # The same thing should work when using a path-like object.
+            version2 = version_for_file_path(pathlib.Path(tmpdir) / 'test')
+            self.assertTrue(len(version2) > 0)
+            # In fact both ways should result in the same version number.
+            self.assertEqual(version1, version2)
+            # Even getting a version number for a directory should not result in
+            # an exception
+            version3 = version_for_file_path(tmpdir)
+            self.assertTrue(len(version3) > 0)
+            # Now we write to the file. In fact, we just open it for writing
+            # because the content does not matter, we just care about creating
+            # it.
+            with open(test_file_path, mode='w'):
+                pass
+            # The version number should have changed now.
+            version4 = version_for_file_path(test_file_path)
+            self.assertTrue(len(version4) > 0)
+            self.assertNotEqual(version1, version4)
+            # We do the same thing again. We actually write something because
+            # simply opening a file for writing might not be enough to update
+            # its modified time. We also sleep for a short amount of time
+            # because the precision of the time stamp stored in the file system
+            # might be limited.
+            time.sleep(1.1)
+            with open(test_file_path, mode='w') as file:
+                file.write('test')
+            version5 = version_for_file_path(test_file_path)
+            self.assertTrue(len(version5) > 0)
+            self.assertNotEqual(version1, version5)
+            self.assertNotEqual(version4, version5)
+
+    def test_version_for_str(self):
+        """
+        Test the `version_for_str` function.
+        """
+        # Getting a version string should even work for an empty string.
+        version1 = version_for_str('')
+        self.assertTrue(len(version1) > 0)
+        # Different strings should result in different versions.
+        version2 = version_for_str('abc')
+        self.assertTrue(len(version2) > 0)
+        self.assertNotEqual(version1, version2)
+        version3 = version_for_str('123')
+        self.assertTrue(len(version3) > 0)
+        self.assertNotEqual(version1, version3)
+        self.assertNotEqual(version2, version3)
+        # Same strings, however, should result in identical versions.
+        version4 = version_for_str('123')
+        self.assertTrue(len(version4) > 0)
+        self.assertEqual(version3, version4)
---
+++ vinegar/utils/version.py
@@ -0,0 +1,104 @@
+"""
+Utility functions for calculating version strings.
+
+Version strings provide a simple tool for finding out whether a specific
+resource has changed.
+
+Internally, they are calculated using the Murmur 3 hash function (if the `mmh3`
+module is available) or the MD5 hash function. Both these hash functions are
+designed in a way that accidental collisions are very unlikely.
+
+Due to the nature of hash functions, a collision can never be avoided with
+absolute certainty, so version strings should only be used when the risk
+associated with using an outdated resource is acceptable.
+"""
+
+import os
+import pathlib
+import sys
+import typing
+
+def aggregate_version(versions: typing.Iterable[str]) -> str:
+    """
+    Calculate an aggregate version from several version strings.
+
+    The aggregate version string is created by calculated a hash over the input
+    version numbers. The hash function is chosen so that accidental collisions
+    are very unlikely.
+
+    :param versions:
+        iterable object that provides ``str`` objects that represent the input
+        version strings.
+    :return:
+        aggregate version string based on the input versions.
+    """
+    return _hash_str('|'.join(versions))
+
+def version_for_file_path(
+        file_path: typing.Union[str, pathlib.PurePath]) -> str:
+    """
+    Return a version string for a file.
+
+    The returned version string is calculated based on the file path and the
+    file's ``ctime`` and ``mtime`` (if the file exists).
+
+    By principle, this function cannot detect when the file content changes
+    without changing the ``ctime`` or ``mtime``. This might happen, if the
+    ``ctime`` and ``mtime`` are deliberately reset to their old values or if
+    the file is changed in rapid succession and the time precision of the
+    underlying file-system is not sufficient to detect that.
+
+    :param file_path:
+        string or `~pathlib.PurePath` instance that represents the path to the
+        file.
+    :return:
+        version string based on the file's ``ctime`` and ``mtime``.
+    """
+    if isinstance(file_path, pathlib.PurePath):
+        file_path = file_path.as_posix()
+    try:
+        file_stat = os.stat(file_path)
+        file_info = 'file_path={0},ctime={1},mtime={2}'.format(
+            file_path, file_stat.st_ctime, file_stat.st_mtime)
+        return _hash_str(file_info)
+    except:
+        # If we cannot stat the file, we calculate the version based on the
+        # file path only. We also encode the exception type, so that a file that
+        # cannot be found has a different version string than a file that exists
+        # but cannot be read.
+        return _hash_str(
+            'file_path={0},exception={1}'.format(
+                file_path, str(sys.exc_info()[0])))
+
+def version_for_str(data: str) -> str:
+    """
+    Returns a version string for a string.
+
+    Effectively, this function simply calculates a hash on the string. This has
+    the effect that different strings should (typically) result in different
+    version strings, unless a hash collision occurs.
+
+    :param data:
+        string to be hashed.
+    :return:
+        hash-based version string.
+    """
+    return _hash_str(data)
+
+# Murmur3 is about twice as fast as MD5, but it is not available in the core
+# Python library. There is a Python-only variant of Murmur3, but that one is
+# actually slower than the built-in MD5. For this reason, we only use Murmur3
+# if the mmh3 package is available on the system. Otherwise, we use MD5.
+try:
+    import mmh3
+
+    def _hash_str(data: str):
+        return mmh3.hash_bytes(data).hex()
+
+except ImportError:
+    import hashlib
+
+    def _hash_str(data: str):
+        hasher = hashlib.md5()
+        hasher.update(data.encode(errors='ignore'))
+        return hasher.hexdigest()
