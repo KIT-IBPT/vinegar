e87efc8470673ecb402d28e20f28d8f52e15750f
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-06-07 12:59:16 +0200

    Add smart dictionary.

---
+++ tests/unit/utils/test_smart_dict.py
@@ -0,0 +1,153 @@
+"""
+Tests for `vinegar.utils.smart_dict`.
+"""
+
+import abc
+import unittest
+
+from vinegar.utils.smart_dict import SmartLookupDict, SmartLookupOrderedDict
+
+class TestSmartLookupDictBase(unittest.TestCase, abc.ABC):
+    """
+    Tests for the `vingear.utils.odict` module.
+    """
+
+    @abc.abstractmethod
+    def new(self, *args, **kwargs):
+        """
+        Create a new instance of the dictionary to be tested.
+        """
+        raise NotImplementedError()
+
+    def test_get(self):
+        """
+        Test the ``get`` method.
+        """
+        d = self.new()
+        # get raises a KeyError if a key cannot be found and no default value
+        # is given. If a default value is given, that value is returned instead.
+        test_value = object()
+        test_value2 = object()
+        with self.assertRaises(KeyError):
+            d.get('key')
+        self.assertEqual(test_value, d.get('key', test_value))
+        # This also applies when the key is nested.
+        with self.assertRaises(KeyError):
+            d.get('key:test')
+        self.assertEqual(test_value, d.get('key:test', test_value))
+        # And it also applies when the key is not found inside the nested dict.
+        d['key'] = {}
+        with self.assertRaises(KeyError):
+            d.get('key:test')
+        self.assertEqual(test_value, d.get('key:test', test_value))
+        # If the key exists, it should be returned, regardless of whether we
+        # give a default value or not.
+        d['key']['test'] = test_value2
+        self.assertEqual(test_value2, d.get('key:test'))
+        self.assertEqual(test_value2, d.get('key:test'), test_value)
+        # A default value can also be given in the form of a keyword argument.
+        self.assertEqual(
+            test_value, d.get('key2:test:test2', default=test_value))
+        # But it cannot be given both as a positional and a keyword argument.
+        with self.assertRaises(TypeError):
+            d.get('key', test_value, default=test_value)
+        # We can choose a different separator.
+        self.assertEqual(test_value2, d.get('key_test', test_value, '_'))
+        self.assertEqual(test_value, d.get('key_test2', test_value, '_'))
+        # That can also be specified as a keyword argument.
+        self.assertEqual(test_value2, d.get('key_test', sep='_'))
+        # And it can be mixed with the default argument.
+        self.assertEqual(test_value2, d.get('key_test', test_value, sep='_'))
+        self.assertEqual(test_value, d.get('key_test2', test_value, sep='_'))
+        self.assertEqual(
+            test_value2, d.get('key_test', default=test_value, sep='_'))
+        self.assertEqual(
+            test_value, d.get('key_test2', default=test_value, sep='_'))
+        # But it cannot be specified as both a positional and a keyword
+        # argument.
+        with self.assertRaises(TypeError):
+            d.get('key_test', test_value, '_', sep='_')
+        # The get method still works for a key that does not contain a
+        # separator.
+        d['abc'] = 123
+        self.assertEqual(123, d.get('abc'))
+        # Even if the key contains the default separator.
+        d['abc:def'] = 456
+        self.assertEqual(456, d.get('abc:def', sep='_'))
+        self.assertEqual(456, d.get('abc:def', sep=None))
+        # We should not be able to look into a non-mapping object using a nested
+        # key, not even if a default value is specified.
+        with self.assertRaises(TypeError):
+            d.get('abc:def')
+        with self.assertRaises(TypeError):
+            d.get('abc:def', test_value)
+        # The method does not take more than three positional arguments.
+        with self.assertRaises(TypeError):
+            d.get('key_test', test_value, '_', 'dummy')
+        # And it does not accept unknown keyword arguments.
+        with self.assertRaises(TypeError):
+            d.get('key_test', dummy='dummy')
+
+    def test_setdefault(self):
+        """
+        Test the ``setdefault`` method.
+        """
+        d = self.new()
+        test_value = object()
+        test_value2 = object()
+        # setdefault should insert a value if the key does not exist yet.
+        self.assertEqual(test_value, d.setdefault('abc', test_value))
+        self.assertEqual(test_value, d['abc'])
+        # But it should not overwrite existing values.
+        self.assertEqual(test_value, d.setdefault('abc', test_value2))
+        self.assertEqual(test_value, d['abc'])
+        # If we specify a nested key, nested dicts should be created as
+        # necessary.
+        self.assertEqual(test_value, d.setdefault('def:ghi:123', test_value))
+        self.assertEqual(test_value, d['def']['ghi']['123'])
+        # If nested dicts already exist, they should be reused.
+        self.assertEqual(test_value, d.setdefault('def:456', test_value))
+        self.assertEqual(
+            {'ghi': {'123': test_value}, '456': test_value}, d['def'])
+        # But this should not work if one of the key components refers to a
+        # value that is not a dict.
+        with self.assertRaises(TypeError):
+            d.setdefault('abc:def', test_value)
+        # We should be able to use a different separator.
+        self.assertEqual(
+            test_value, d.setdefault('def_ghi_123', test_value2, sep='_'))
+        self.assertEqual(
+            test_value2, d.setdefault('def_ghi_456', test_value2, sep='_'))
+        self.assertEqual(test_value2, d['def']['ghi']['456'])
+
+class TestSmartLookupDict(TestSmartLookupDictBase):
+
+    def new(self, *args, **kwargs):
+        """
+        Create a new instance of `SmartLookupDict`.
+        """
+        return SmartLookupDict(*args, **kwargs)
+
+class TestSmartLookupOrderedDict(TestSmartLookupDictBase):
+
+    def new(self, *args, **kwargs):
+        """
+        Create a new instance of `SmartLookupOrderedDict`.
+        """
+        return SmartLookupOrderedDict(*args, **kwargs)
+
+    def test_preserves_order(self):
+        """
+        Test that the ``SmartLookupOrderedDict`` does in fact preserve the
+        insertion order.
+        """
+        d = SmartLookupOrderedDict()
+        l = [5, 1, 12, 2, 3, 6, 4]
+        for i in l:
+            d[i] = i
+        self.assertEqual(l, list(d.keys()))
+        self.assertEqual(l, list(d.values()))
+
+# We have to delete the base class because it cannot be instantiated and the
+# automatic test discovery would try to instantiate it.
+del TestSmartLookupDictBase
---
+++ vinegar/utils/smart_dict.py
@@ -0,0 +1,246 @@
+"""
+Provides a dictionary that has a smart ``get`` method.
+
+The ``get`` method of ``SmartLookupDict`` and ``SmartLookupOrderedDict`` is
+implemented in a way that makes it easy to look for values in nested
+dictionaries.
+
+For example, take a dictionary with the following content::
+
+    {'key1': {'key2': {'key3': 'value'}}}
+
+In order to get the nested value, one would ordinarily have to use one of the
+two following expressions::
+
+    value = regular_dict['key1']['key2']['key3']
+    value = regular_dict.get('key1').get('key2').get('key3')
+
+This is particularly bothersome if one cannot be sure that the nested value even
+exists. In order to use a default value if one of the keys is missing, one could
+use nested calls to ``get`` with default values::
+
+    value = regular_dict.get(
+        'key1', {}).get('key2', {}).get('key3', 'default value')
+
+An alternative would be catching the ``KeyError``::
+
+    try:
+        value = regular_dict['key1']['key2']['key3']
+    catch KeyError:
+        value = "default value"
+
+Both alternatives are not very user friendly, in particular if used from a
+template language where a ``try...catch`` construct might not be available.
+
+The smart dictionaries make such use cases either by enabling the user to get a
+nested value in a single call to ``get``::
+
+    value = smart_dict.get('key1:key2:key3', 'default value')
+
+By default the colon (``:``) is used as the key separator, but this can be
+changed by specifying the ``sep`` argument::
+
+    value = smart_dict.get('key1_key2_key3', 'default value', sep='_')
+
+In addition to the ``get`` method, the ``setdefault`` method is overriden, so
+that it automatically inserts nested dictionaries if needed.
+
+The implementation of ``get`` provided by the smart lookup dictionaries also
+differs from the one for regular dictionaries in that it do not use a default
+value of ``None`` by default. Instead, it raises a ``KeyError`` if the key is
+not found and no default value is provided.
+
+The are two versions of the smart lookup dict:
+
+`SmartLookupDict` uses ``dict`` as its base class. ``SmartLookupOrderedDict`` on
+the other hand uses ``vinegar.utils.odict.OrderedDict`` as its base class. This
+ensures that the dictionary preserves the insertion order of keys (see
+`vinegar.utils.odict` for details).
+"""
+
+from vinegar.utils.odict import OrderedDict
+
+class SmartLookupDict(dict):
+
+    def get(self, key, *args, **kwargs):
+        """
+        Return the value for ``key``.
+
+        If ``key`` is not in the dictionary and ``default`` is given, return
+        ``default``. If ``default`` is not given raise a ``KeyError``.
+
+        The ``key`` can be a nested key into nested dictionaries inside this
+        dictionary. The ``key`` is split using the ``sep`` (default ``:``) and
+        each component is used as the key on one level of nested dictionaries,
+        the first component being used at the top level.
+
+        :param key:
+            key for which the value shall be looked up. This can be a nested key
+            into nested dictionaries. ``sep`` is used to separate components of
+            the key.
+        :param default:
+            default value to be returned if this dictionary (or one of the
+            nested dictionaries) does not contain ``key``. If not specified, a
+            ``KeyError`` is raised instead.
+        :param sep:
+            separator separating components in ``key``. The default is ``:``.
+        :return:
+            value for ``key`` or ``default`` if specified and ``key`` is not
+            found.
+        """
+        return _smart_get(self, key, *args, **kwargs)
+
+    def setdefault(self, key, default=None, sep=':', dict_type=dict):
+        """
+        Return the value for ``key`` inserting and returning ``default`` if
+        ``key`` does not exist yet.
+
+        The ``key`` can be a nested key into nested dictionaries inside this
+        dictionary. The ``key`` is split using the ``sep`` (default ``:``) and
+        each component is used as the key on one level of nested dictionaries,
+        the first component being used at the top level.
+
+        If one of the key components except the last key component is not found,
+        a new dictionary of type ``dict_type`` is inserted automatically.
+
+        :param key:
+            key for which the value shall be looked up. This can be a nested key
+            into nested dictionaries. ``sep`` is used to separate components of
+            the key.
+        :param default:
+            default value to be inserted and returned if this dictionary (or one
+            of the nested dictionaries) does not contain ``key``.
+        :param dict_type:
+            the type of dictionary that is created when a new dictionary has to
+            be inserted. The default is ``dict``.
+        :return:
+            value for ``key`` or ``default`` if ``key`` is not found.
+        """
+        return _smart_setdefault(self, key, default, sep, dict)
+
+class SmartLookupOrderedDict(OrderedDict):
+
+    def get(self, key, *args, **kwargs):
+        """
+        Return the value for ``key``.
+
+        If ``key`` is not in the dictionary and ``default`` is given, return
+        ``default``. If ``default`` is not given raise a ``KeyError``.
+
+        The ``key`` can be a nested key into nested dictionaries inside this
+        dictionary. The ``key`` is split using the ``sep`` (default ``:``) and
+        each component is used as the key on one level of nested dictionaries,
+        the first component being used at the top level.
+
+        :param key:
+            key for which the value shall be looked up. This can be a nested key
+            into nested dictionaries. ``sep`` is used to separate components of
+            the key.
+        :param default:
+            default value to be returned if this dictionary (or one of the
+            nested dictionaries) does not contain ``key``. If not specified, a
+            ``KeyError`` is raised instead.
+        :param sep:
+            separator separating components in ``key``. The default is ``:``.
+        :return:
+            value for ``key`` or ``default`` if specified and ``key`` is not
+            found.
+        """
+        return _smart_get(self, key, *args, **kwargs)
+
+    def setdefault(self, key, default=None, sep=':', dict_type=dict):
+        """
+        Return the value for ``key`` inserting and returning ``default`` if
+        ``key`` does not exist yet.
+
+        The ``key`` can be a nested key into nested dictionaries inside this
+        dictionary. The ``key`` is split using the ``sep`` (default ``:``) and
+        each component is used as the key on one level of nested dictionaries,
+        the first component being used at the top level.
+
+        If one of the key components except the last key component is not found,
+        a new dictionary of type ``dict_type`` is inserted automatically.
+
+        :param key:
+            key for which the value shall be looked up. This can be a nested key
+            into nested dictionaries. ``sep`` is used to separate components of
+            the key.
+        :param default:
+            default value to be inserted and returned if this dictionary (or one
+            of the nested dictionaries) does not contain ``key``.
+        :param dict_type:
+            the type of dictionary that is created when a new dictionary has to
+            be inserted. The default is ``vinegar.utils.odict.OrderedDict``.
+        :return:
+            value for ``key`` or ``default`` if ``key`` is not found.
+        """
+        return _smart_setdefault(self, key, default, sep, OrderedDict)
+
+def _smart_get(d, key, *args, **kwargs):
+    """
+    Actual implementation of ``get`` shared by ``SmartLookupDict`` and
+    ``SmartLookupOrderedDict``.
+    """
+    have_default = False
+    sep = ':'
+    if len(args) > 2:
+        raise TypeError(
+            'get expected at most 3 arguments, got %d' % (len(args) + 1))
+    if len(args) >= 1:
+        if 'default' in kwargs:
+            raise TypeError(
+                'Default value must not be given both as a positional and a '
+                'keyword argument.')
+        default = args[0]
+        have_default = True
+    if len(args) >= 2:
+        if 'sep' in kwargs:
+            raise TypeError(
+                'Separator must not be given both as a positional argument and '
+                'a keyword argument.')
+        sep = args[1]
+    try:
+        default = kwargs['default']
+        have_default = True
+    except KeyError:
+        pass
+    try:
+        sep = kwargs['sep']
+    except KeyError:
+        pass
+    for kwarg_key in kwargs:
+        if kwarg_key not in ('default', 'sep'):
+            raise TypeError(
+                'test() got an unexpected keyword argument \'%s\'' % kwarg_key)
+    keys = key.split(sep)
+    nested_value = d
+    try:
+        for key_part in keys:
+            nested_value = nested_value[key_part]
+    except KeyError:
+        if have_default:
+            return default
+        else:
+            raise KeyError(key) from None
+    return nested_value
+
+def _smart_setdefault(d, key, default, sep, dict_type):
+    """
+    Actual implementation of ``setdefault`` shared by ``SmartLookupDict`` and
+    ``SmartLookupOrderedDict``.
+    """
+    keys = key.split(sep)
+    last_key = keys[-1]
+    keys = keys[:-1]
+    nested_value = d
+    for key_part in keys:
+        try:
+            nested_value = nested_value[key_part]
+        except KeyError:
+            nested_value[key_part] = dict_type()
+            nested_value = nested_value[key_part]
+    try:
+        return nested_value[last_key]
+    except KeyError:
+        nested_value[last_key] = default
+        return default
