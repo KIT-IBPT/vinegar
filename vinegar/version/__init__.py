e39ac6201cea9c1b8a2662e3d7597da268db886a
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-06-11 22:04:09 +0200

    Add version info to Python.

---
+++ vinegar/version/__init__.py
@@ -0,0 +1,9 @@
+"""
+Provides the version of the Vinegar distribution.
+"""
+
+# Version (as a tuple)
+version = (0, 0, 1)
+
+# Version (as a string)
+version_string = '.'.join([str(ver_component) for ver_component in version])
