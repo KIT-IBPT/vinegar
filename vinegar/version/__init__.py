569d21b7c5d16113c97c13b977212c5569894ef6
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-06-12 20:10:42 +0200

    Mark comments in version modules as doc comments.
    This is necessary so that they are recognized by Sphinx.

--- vinegar/version/__init__.py
+++ vinegar/version/__init__.py
@@ -2,8 +2,8 @@
 Provides the version of the Vinegar distribution.
 """
 
-# Version (as a tuple)
+#: Version (as a tuple)
 version = (0, 0, 1)
 
-# Version (as a string)
+#: Version (as a string)
 version_string = '.'.join([str(ver_component) for ver_component in version])
