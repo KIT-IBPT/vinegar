23a0a3bfaa85e91fc474e154b175d629dd8d155e
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-06-25 19:04:07 +0200

    Use default theme when building on readthedocs.io.

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,8 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import vinegar.version
 
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+
 # -- Project information -----------------------------------------------------
 
 project = 'Vinegar Boot'
@@ -64,7 +66,10 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'nature'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
