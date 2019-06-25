402ed4cfefc859610f124491d6d145c5993c0b43
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2019-06-25 19:45:03 +0200

    Release version 1.0.0.

--- vinegar/version/__init__.py
+++ vinegar/version/__init__.py
@@ -14,7 +14,7 @@ BETA_VERSION_OFFSET = -1000
 #:
 #: When changing the version here, it also has to be changed in
 #: debian/changelog.
-VERSION = (1, 0, 1 + BETA_VERSION_OFFSET)
+VERSION = (1, 0, 0)
 
 
 def _version_string():
