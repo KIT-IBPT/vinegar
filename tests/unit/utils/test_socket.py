02c4f696b2596dabf0ba5a29a9716b42dc2c091e
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2023-04-20 21:52:23 +0200

    Add vinegar.utils.socket.contains_ip_address.

--- tests/unit/utils/test_socket.py
+++ tests/unit/utils/test_socket.py
@@ -4,7 +4,11 @@ Tests for `vinegar.utils.socket`.
 
 import unittest
 
-from vinegar.utils.socket import ipv6_address_unwrap, socket_address_to_str
+from vinegar.utils.socket import (
+    contains_ip_address,
+    ipv6_address_unwrap,
+    socket_address_to_str,
+)
 
 
 class TestSocket(unittest.TestCase):
@@ -12,32 +16,325 @@ class TestSocket(unittest.TestCase):
     Tests for the `vingear.utils.socket` module.
     """
 
+    def test_contains_ip_address_empty_list(self):
+        """
+        Test the ``contains_ip_address`` function (with an empty list).
+        """
+        # An empty list should not cause an exception.
+        self.assertFalse(contains_ip_address([], "192.168.0.1"))
+        self.assertFalse(contains_ip_address([], "::1", True))
+
+    def test_contains_ip_address_malformed(self):
+        """
+        Test the ``contains_ip_address`` function with malformed input.
+        """
+        # A malformed IP address (whether it is in the set of IP addresses or
+        # the IP address to be tested) should only cause an exception if the
+        # raise_error_if_malformed flag is set.
+        self.assertFalse(contains_ip_address([], "127.0.0.1/8"))
+        with self.assertRaises(ValueError):
+            contains_ip_address(
+                [], "127.0.0.1/8", raise_error_if_malformed=True
+            )
+        self.assertFalse(contains_ip_address([], "abcd"))
+        with self.assertRaises(ValueError):
+            contains_ip_address([], "abcd", raise_error_if_malformed=True)
+        self.assertFalse(contains_ip_address(["abcd"], "127.0.0.1"))
+        with self.assertRaises(ValueError):
+            contains_ip_address(
+                ["abcd"],
+                "127.0.0.1",
+                raise_error_if_malformed=True,
+            )
+        # This is malformed because we did not allow a netmask. When the
+        # raise_error_if_malformed flag is set, this should return False
+        # because the entry with the netmask is ignored (even though it would
+        # match).
+        self.assertFalse(
+            contains_ip_address(
+                ["127.0.0.1/32"], "127.0.0.1", allow_netmask=False
+            )
+        )
+        with self.assertRaises(ValueError):
+            contains_ip_address(
+                ["127.0.0.1/8"],
+                "127.0.0.1",
+                allow_netmask=False,
+                raise_error_if_malformed=True,
+            )
+
+    def test_contains_ip_address_no_netmask(self):
+        """
+        Test the ``contains_ip_address`` function with ``allow_netmask=False``.
+        """
+        # We specify entries with a netmask here, even though we test with
+        # allow_netmask=False. These entries should simply be ignored.
+        ip_addresses = [
+            "192.168.1.0/24",
+            "10.0.37.35/27",
+            "10.1.0.25/32",
+            "10.2.3.27",
+            "10.4.2.31",
+            "fc00::ab:0/112",
+            "fc00::13:1/110",
+            "fc00::1234/128",
+            "fc00::c5:4",
+            "fc00::1234:d3:abcd",
+            "::ffff:10.27.0.4/112",
+            "::ffff:10.117.32.3",
+        ]
+        # Tests for 10.2.3.27.
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "10.2.3.27", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.2.3.26", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.2.3.28", allow_netmask=False)
+        )
+        # Tests for 10.4.2.31.
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "10.4.2.31", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.4.2.30", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.4.2.32", allow_netmask=False)
+        )
+        # Tests for fc00::c5:4.
+        self.assertTrue(
+            contains_ip_address(
+                ip_addresses, "fc00::c5:4", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::c5:3", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::c5:5", allow_netmask=False
+            )
+        )
+        # Tests for fc00::1234:d3:abcd.
+        self.assertTrue(
+            contains_ip_address(
+                ip_addresses, "fc00::1234:d3:abcd", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::1234:d3:abcc", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::1234:d3:abce", allow_netmask=False
+            )
+        )
+        # Tests for ::ffff:10.117.32.3.
+        self.assertTrue(
+            contains_ip_address(
+                ip_addresses, "10.117.32.3", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "10.117.32.2", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "10.117.32.4", allow_netmask=False
+            )
+        )
+        # Tests with IPv4-mapped IPv6 addresses.
+        self.assertTrue(
+            contains_ip_address(
+                ip_addresses, "::ffff:10.117.32.3", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "::ffff:10.117.32.2", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "::ffff:10.117.32.4", allow_netmask=False
+            )
+        )
+        # The entries that specify a subnet mask should be ignored, so they
+        # should not produce any matches.
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "192.168.1.0", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "10.0.37.32", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.1.0.25", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::ab:0", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::12:0", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "fc00::1234", allow_netmask=False
+            )
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "10.27.0.0", allow_netmask=False)
+        )
+        self.assertFalse(
+            contains_ip_address(
+                ip_addresses, "::ffff:192.168.1.0", allow_netmask=False
+            )
+        )
+
+    def test_contains_ip_address_with_netmask(self):
+        """
+        Test the ``contains_ip_address`` function with ``allow_netmask=True``.
+        """
+        ip_addresses = [
+            "192.168.1.0/24",
+            "10.0.37.35/27",
+            "10.1.0.25/32",
+            "10.2.3.27",
+            "10.4.2.31",
+            "fc00::ab:0/112",
+            "fc00::13:1/110",
+            "fc00::1234/128",
+            "fc00::c5:4",
+            "fc00::1234:d3:abcd",
+            "::ffff:10.27.0.4/112",
+            "::ffff:10.117.32.3",
+        ]
+        # Tests for 192.168.1.0/24.
+        self.assertTrue(contains_ip_address(ip_addresses, "192.168.1.0"))
+        self.assertTrue(contains_ip_address(ip_addresses, "192.168.1.255"))
+        self.assertFalse(contains_ip_address(ip_addresses, "192.168.0.255"))
+        self.assertFalse(contains_ip_address(ip_addresses, "192.168.2.0"))
+        # Tests for 10.0.37.35/27.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.0.37.32"))
+        self.assertTrue(contains_ip_address(ip_addresses, "10.0.37.63"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.0.37.31"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.0.37.64"))
+        # Tests for 10.1.0.25/32.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.1.0.25"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.1.0.24"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.1.0.26"))
+        # Tests for 10.2.3.27.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.2.3.27"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.2.3.26"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.2.3.28"))
+        # Tests for 10.4.2.31.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.4.2.31"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.4.2.30"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.4.2.32"))
+        # Tests fc00::ab:0/112.
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::ab:0"))
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::ab:ffff"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::aa:ffff"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::ac:0"))
+        # Tests fc00::13:1/110.
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::10:0"))
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::13:ffff"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::0f:ffff"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::14:0"))
+        # Tests for fc00::1234/128.
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::1234"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::1233"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::1235"))
+        # Tests for fc00::c5:4.
+        self.assertTrue(contains_ip_address(ip_addresses, "fc00::c5:4"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::c5:3"))
+        self.assertFalse(contains_ip_address(ip_addresses, "fc00::c5:5"))
+        # Tests for fc00::1234:d3:abcd.
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "fc00::1234:d3:abcd")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "fc00::1234:d3:abcc")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "fc00::1234:d3:abce")
+        )
+        # Tests for ::ffff:10.27.0.4/112.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.27.0.0"))
+        self.assertTrue(contains_ip_address(ip_addresses, "10.27.255.255"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.26.255.255"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.28.0.0"))
+        # Tests for ::ffff:10.117.32.3.
+        self.assertTrue(contains_ip_address(ip_addresses, "10.117.32.3"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.117.32.2"))
+        self.assertFalse(contains_ip_address(ip_addresses, "10.117.32.4"))
+        # Tests with IPv4-mapped IPv6 addresses.
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "::ffff:192.168.1.0")
+        )
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "::ffff:192.168.1.255")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "::ffff:192.168.0.255")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "::ffff:192.168.2.0")
+        )
+        self.assertTrue(
+            contains_ip_address(ip_addresses, "::ffff:10.117.32.3")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "::ffff:10.117.32.2")
+        )
+        self.assertFalse(
+            contains_ip_address(ip_addresses, "::ffff:10.117.32.4")
+        )
+
     def test_ipv6_address_unwrap(self):
         """
         Test the ``ipv6_address_unwrap`` function.
         """
         # Regularly formatted IPv4-mapped IPv6 addresses.
-        self.assertEqual("1.2.3.4", ipv6_address_unwrap('::ffff:1.2.3.4'))
-        self.assertEqual("127.0.0.1", ipv6_address_unwrap('::FFFF:127.0.0.1'))
+        self.assertEqual("1.2.3.4", ipv6_address_unwrap("::ffff:1.2.3.4"))
+        self.assertEqual("127.0.0.1", ipv6_address_unwrap("::FFFF:127.0.0.1"))
         # Unusually formatted IPv4-mapped IPv6 addresses.
-        self.assertEqual("1.2.3.4", ipv6_address_unwrap('::ffff:0102:0304'))
-        self.assertEqual("127.0.0.1", ipv6_address_unwrap('::FfFf:7f00:1'))
+        self.assertEqual("1.2.3.4", ipv6_address_unwrap("::ffff:0102:0304"))
+        self.assertEqual("127.0.0.1", ipv6_address_unwrap("::FfFf:7f00:1"))
         # Strings that are not IPv4-mapped IPv6 addresses
-        self.assertEqual("abc", ipv6_address_unwrap('abc'))
-        self.assertEqual("fc00::abcd", ipv6_address_unwrap('fc00::abcd'))
+        self.assertEqual("abc", ipv6_address_unwrap("abc"))
+        self.assertEqual("fc00::abcd", ipv6_address_unwrap("fc00::abcd"))
 
     def test_socket_address_to_str(self):
         """
         Test the ``socket_address_to_str`` function.
         """
         # IP address only.
-        self.assertEqual("127.0.0.1", socket_address_to_str(('127.0.0.1',)))
-        self.assertEqual("1.2.3.4", socket_address_to_str(('::ffff:1.2.3.4',)))
-        self.assertEqual("fc00::1234", socket_address_to_str(('fc00::1234',)))
+        self.assertEqual("127.0.0.1", socket_address_to_str(("127.0.0.1",)))
+        self.assertEqual("1.2.3.4", socket_address_to_str(("::ffff:1.2.3.4",)))
+        self.assertEqual("fc00::1234", socket_address_to_str(("fc00::1234",)))
         # IP address and port number.
         self.assertEqual(
-            "127.0.0.1:123", socket_address_to_str(('127.0.0.1:123',)))
+            "127.0.0.1:123", socket_address_to_str(("127.0.0.1:123",))
+        )
         self.assertEqual(
-            "1.2.3.4:456", socket_address_to_str(('::ffff:1.2.3.4', 456)))
+            "1.2.3.4:456", socket_address_to_str(("::ffff:1.2.3.4", 456))
+        )
         self.assertEqual(
-            "[fc00::1234]:789", socket_address_to_str(('fc00::1234', 789)))
+            "[fc00::1234]:789", socket_address_to_str(("fc00::1234", 789))
+        )
--- vinegar/utils/socket.py
+++ vinegar/utils/socket.py
@@ -1,9 +1,174 @@
 """
-Utilities for dealing with sockets.
+Utilities for dealing with sockets and IP addresses.
 """
 
+import re
 import socket
 import typing
+
+# Regular expression that we use for matching the netmask portion of an IP
+# address with a netmask.
+_NETMASK_REGEXP = re.compile("[0-9]+")
+
+
+def _ip_address_in_subnet(
+        ip_address_bytes: bytes,
+        network_address_bytes: bytes,
+        netmask_bits: int) -> bool:
+    assert len(ip_address_bytes) == len(network_address_bytes)
+    assert netmask_bits <= (len(ip_address_bytes) * 8)
+    # First, we compare the full bytes of the two addresses.
+    whole_bytes = netmask_bits // 8
+    if ip_address_bytes[:whole_bytes] != network_address_bytes[:whole_bytes]:
+        return False
+    # Second, we compare the relevant bits of the last relevant byte.
+    remaining_bits = netmask_bits % 8
+    if remaining_bits == 0:
+        return True
+    ip_address_byte = ip_address_bytes[whole_bytes]
+    network_address_byte = network_address_bytes[whole_bytes]
+    byte_mask = 256 - (1 << (8 - remaining_bits))
+    return (ip_address_byte & byte_mask) == (network_address_byte & byte_mask)
+
+
+def _parse_ip_address(ip_address: str, allow_netmask: bool) -> typing.Tuple[
+        int, bytes, int]:
+    original_ip_address = ip_address
+    if allow_netmask and '/' in ip_address:
+        ip_address, netmask = ip_address.rsplit('/', 1)
+        if _NETMASK_REGEXP.fullmatch(netmask):
+            netmask = int(netmask)
+        else:
+            ip_address = original_ip_address
+            netmask = None
+    else:
+        netmask = None
+    # First, we try to interpret the address as an IPv4 address.
+    try:
+        ip_address_bytes = socket.inet_pton(socket.AF_INET, ip_address)
+        if netmask is None:
+            netmask = 32
+        elif netmask > 32:
+            raise ValueError(
+                'Error parsing "{}": Invalid netmask.'.format(
+                    original_ip_address))
+        return socket.AF_INET, ip_address_bytes, netmask
+    except OSError:
+        # If there is an error, this most likely means that we are dealing with
+        # an IPv6 address.
+        pass
+    try:
+        ip_address_bytes = socket.inet_pton(socket.AF_INET6, ip_address)
+        if netmask is None:
+            netmask = 128
+        elif netmask > 128:
+            raise ValueError(
+                'Error parsing "{}": Invalid netmask.'.format(
+                    original_ip_address))
+        return socket.AF_INET6, ip_address_bytes, netmask
+    except OSError as err:
+        # The address is neither a valid IPv4 nor a valid IPv6 address.
+        raise ValueError(
+            'Error parsing "{}": This neither is a valid IPv4 nor IPv6 '
+            'address.'.format(original_ip_address)) from err
+
+
+def _parse_ip_address_split_ipv4_ipv6(ip_address: str) -> typing.Tuple[
+            typing.Optional[bytes],
+            bytes]:
+    ip_address_family, ip_address_bytes, _ = _parse_ip_address(
+        ip_address, False)
+    # If the IP address is an IPv4 address, we also convert it to an
+    # IPv4-mapped IPv6 address in order to be able to correctly match it
+    # against IPv6 candidate addresses. If it is an IPv4-mapped IPv6 address,
+    # we also convert it to an IPv4 address in order to correctly match it
+    # against IPv4 candidate addresses.
+    if ip_address_family == socket.AF_INET:
+        ip_address_bytes_ipv4 = ip_address_bytes
+        ip_address_bytes_ipv6 = (
+            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff'
+            + ip_address_bytes)
+    elif ip_address_bytes.startswith(
+            b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff'):
+        ip_address_bytes_ipv4 = ip_address_bytes[-4:]
+        ip_address_bytes_ipv6 = ip_address_bytes
+    else:
+        ip_address_bytes_ipv4 = None
+        ip_address_bytes_ipv6 = ip_address_bytes
+    return ip_address_bytes_ipv4, ip_address_bytes_ipv6
+
+
+def contains_ip_address(
+        ip_address_set: typing.Collection[str],
+        ip_address: str,
+        allow_netmask: bool = True,
+        raise_error_if_malformed: bool = False) -> bool:
+    """
+    Check whether an IP address is contained in a set of IP addresses.
+
+    This works with both IPv4 and IPv6 addresses. Optionally, the set of IP
+    addresses may contain address ranges in CIDR notation (e.g.
+    192.168.0.0/24).
+
+    This function supports IPv4-mapped IPv6 addresses. Such addresses are
+    treated like the IPv4 address that they represent.
+
+    :param ip_address_set:
+        list or set of IP addresses with which the  ``ip_address`` shall be
+        compared. If ``allow_netmask`` is ``True``, this may include ranges of
+        IP addresses specified using the CIDR notation.
+    :param ip_address:
+        IP address that shall be tested. This can be an IPv4 or an IPv6
+        address.
+    :param allow_netmask:
+        ``True`` if ip_address_set may contain address ranges, ``False`` if it
+        may only contain single IP addresses. The default is ``True``.
+    :param raise_error_if_malformed:
+        ``True`` if a malformed IP address in ``ip_address_set` or
+        ``ip_address`` should result in a ``ValueError`` being raised.
+        ``False`` if it should result in the entry being ignored (for
+        ``ip_address_set``) or ``False`` being returned (for ``ip_address``).
+    :return:
+        ``True`` if ``ip_address`` is contained in ``ip_address_set``,
+        ``False`` otherwise.
+    """
+    # We parse the IP address, returning the byte sequence both as an IPv4
+    # address and an IPv4-mapped IPv6 address. If the original address is an
+    # IPv6 address that is not an IPv4-mapped IPv6 address, the IPv4 address is
+    # None.
+    try:
+        ip_address_bytes_ipv4, ip_address_bytes_ipv6 = (
+            _parse_ip_address_split_ipv4_ipv6(ip_address))
+    except ValueError:
+        if raise_error_if_malformed:
+            raise
+        return False
+    # Now, we can compare with all candidates until we find a match.
+    for candidate_ip_address in ip_address_set:
+        try:
+            (
+                candidate_ip_address_family,
+                candidate_ip_address_bytes,
+                candidate_ip_address_netmask) = _parse_ip_address(
+                    candidate_ip_address, allow_netmask)
+        except ValueError:
+            if raise_error_if_malformed:
+                raise
+            continue
+        if (candidate_ip_address_family == socket.AF_INET
+                and ip_address_bytes_ipv4):
+            if _ip_address_in_subnet(
+                    ip_address_bytes_ipv4,
+                    candidate_ip_address_bytes,
+                    candidate_ip_address_netmask):
+                return True
+        elif candidate_ip_address_family == socket.AF_INET6:
+            if _ip_address_in_subnet(
+                    ip_address_bytes_ipv6,
+                    candidate_ip_address_bytes,
+                    candidate_ip_address_netmask):
+                return True
+    return False
 
 
 def ipv6_address_unwrap(ipv6_address: str) -> str:
