3eac6799f74b05e299066e5ec2395e7030acd24b
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2023-05-31 13:20:58 +0200

    Bump version to 2.0beta1.
    We are going to introduce changes that break backwards compatibility, so
    incrementing the major version is in order.

--- vinegar/version/__init__.py
+++ vinegar/version/__init__.py
@@ -14,7 +14,7 @@ BETA_VERSION_OFFSET = -1000
 #:
 #: When changing the version here, it also has to be changed in
 #: debian/changelog.
-VERSION = (1, 3, 1 + BETA_VERSION_OFFSET)
+VERSION = (2, 0, 1 + BETA_VERSION_OFFSET)
 
 
 def _version_string():
