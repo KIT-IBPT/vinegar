a73c184aff07dca1d2c436beacd7f6df177cf9c9
Sebastian Marsching <sebastian.marsching-git-2016@aquenos.com>
2023-06-23 12:11:01 +0200

    Change versioning scheme for beta versions.
    The new scheme better fits the styles used by both DPKG and PIP.

--- vinegar/version/__init__.py
+++ vinegar/version/__init__.py
@@ -5,9 +5,9 @@ Provides the version of the Vinegar distribution.
 # For a beta version, we use a negative offset for the last component of the
 # version number. This has two consequences: First, a beta version always
 # compares less than the final version. Second, we can detect this when
-# generating the version string and insert a "beta" in the string. For example,
-# a version of (1, 0, 1 + BETA_VERSION_OFFSET) will result in the version
-# string "1.0beta1".
+# generating the version string and insert a "b" in the string. For example, a
+# version of (1, 0, 1 + BETA_VERSION_OFFSET) will result in the version string
+# "1.0b1".
 BETA_VERSION_OFFSET = -1000
 
 #: Version (as a tuple).
@@ -28,7 +28,7 @@ def _version_string():
         else:
             # We assume that there is only one negative component in the
             # version number.
-            version_string += f"beta{component - BETA_VERSION_OFFSET}"
+            version_string += f"b{component - BETA_VERSION_OFFSET}"
     return version_string[1:]
 
 
